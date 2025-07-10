913e834555ffa4fc964cb90aab16bef7e66a3f40
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-07-10 23:36:59 +0100

    Cleans up blueprint registrations

--- app/example_form/__init__.py
+++ app/example_form/__init__.py
@@ -3,7 +3,7 @@ from app.utils import register_form_view
 from .forms import ExampleForm
 from .views import ExampleFormView
 
-bp = Blueprint("example_form", __name__, template_folder="../templates/main")
+bp = Blueprint("example_form", __name__)
 
 register_form_view(form_class=ExampleForm, view_class=ExampleFormView, blueprint=bp)
 from .routes import *
