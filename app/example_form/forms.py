8c86d068204a9420048fa2094ab96b97e704ed1c
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-07-10 23:28:40 +0100

    Update example form to use new parametrised Widgets

--- app/example_form/forms.py
+++ app/example_form/forms.py
@@ -1,17 +1,16 @@
 from wtforms.fields.simple import StringField
 from wtforms.validators import InputRequired
-from govuk_frontend_wtf.wtforms_widgets import GovTextInput
 from ..forms import BaseForm
+from ..widgets import PageHeadingInput
 
 
 class ExampleForm(BaseForm):
-    template = "example-form.html"
     title = "Form Title"
     url = "example-form"
 
     full_name = StringField(
         "Your full name",
-        widget=GovTextInput(),
+        widget=PageHeadingInput(hint="This should include any middle names."),
         validators=[
             InputRequired(message="Enter your name"),
         ],
