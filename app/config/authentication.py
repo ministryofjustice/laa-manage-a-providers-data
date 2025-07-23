e9900f12f472a8b76f7edc54a92811443a2570f9
said-moj <45761276+said-moj@users.noreply.github.com>
2025-07-23 16:51:58 +0100

    Add entra-id config

--- app/config/authentication.py
+++ app/config/authentication.py
@@ -4,6 +4,13 @@ import os
 class AuthenticationConfig:
     """Configuration class for Entra ID authentication settings."""
     CLIENT_ID = os.getenv("CLIENT_ID")
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    CLIENT_SECRET_THUMBPRINT = os.environ.get("CLIENT_SECRET_THUMBPRINT")
+    if CLIENT_SECRET_THUMBPRINT:
+        CLIENT_SECRET = {
+            "private_key": os.getenv("CLIENT_SECRET"),
+            "thumbprint": CLIENT_SECRET_THUMBPRINT,
+        }
+    else:
+        CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
     AUTHORITY = os.getenv("AUTHORITY")
     REDIRECT_URI = os.getenv("REDIRECT_URI")
