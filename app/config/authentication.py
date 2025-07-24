02371d519ea19ccabfe65603080b5efb22f7424f
said-moj <45761276+said-moj@users.noreply.github.com>
2025-07-24 09:23:35 +0100

    Prefix entra id environment variables with ENTRA_ID

--- app/config/authentication.py
+++ app/config/authentication.py
@@ -3,14 +3,14 @@ import os
 
 class AuthenticationConfig:
     """Configuration class for Entra ID authentication settings."""
-    CLIENT_ID = os.getenv("CLIENT_ID")
-    CLIENT_SECRET_THUMBPRINT = os.environ.get("CLIENT_SECRET_THUMBPRINT")
+    CLIENT_ID = os.getenv("ENTRA_ID_CLIENT_ID")
+    CLIENT_SECRET_THUMBPRINT = os.environ.get("ENTRA_ID_CLIENT_SECRET_THUMBPRINT")
     if CLIENT_SECRET_THUMBPRINT:
         CLIENT_SECRET = {
-            "private_key": os.getenv("CLIENT_SECRET"),
+            "private_key": os.getenv("ENTRA_ID_CLIENT_SECRET"),
             "thumbprint": CLIENT_SECRET_THUMBPRINT,
         }
     else:
-        CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-    AUTHORITY = os.getenv("AUTHORITY")
-    REDIRECT_URI = os.getenv("REDIRECT_URI")
+        CLIENT_SECRET = os.environ.get("ENTRA_ID_CLIENT_SECRET")
+    AUTHORITY = os.getenv("ENTRA_ID_AUTHORITY")
+    REDIRECT_URI = os.getenv("ENTRA_ID_REDIRECT_URI")
