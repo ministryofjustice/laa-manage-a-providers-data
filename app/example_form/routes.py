438334497b09eb21b561745f24d0aecdd695310b
said-moj <45761276+said-moj@users.noreply.github.com>
2025-07-25 16:20:13 +0100

    Remove manually passing the user to the template. This is part of the current context and does not explicitly need to be passed

--- app/example_form/routes.py
+++ app/example_form/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, session
 
-from . import bp
 from app import auth
+
+from . import bp
 
 
 @bp.get("/success")
 @auth.login_required
 def success(*, context):
     name = session.get("full_name")
-    return render_template("success.html", name=name, user=context["user"])
+    return render_template("success.html", name=name)
