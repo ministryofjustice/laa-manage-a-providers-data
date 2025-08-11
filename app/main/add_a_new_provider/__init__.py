b6e58248f0ff60efc869dae8763540e8a2916783
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-08-11 21:44:50 +0100

    Register advocate details view

--- app/main/add_a_new_provider/__init__.py
+++ app/main/add_a_new_provider/__init__.py
@@ -1,14 +1,22 @@
+from app.main import bp
 from app.utils import register_form_view
 
-from .forms import AddProviderForm, ChambersDetailsForm, LspDetailsForm, ParentProviderForm
-from .views import AddProviderFormView, ChambersDetailsFormView, LspDetailsFormView, ParentProviderFormView
-from app.main import bp
+from .forms import AddProviderForm, AdvocateDetailsForm, ChambersDetailsForm, LspDetailsForm, ParentProviderForm
+from .views import (
+    AddProviderFormView,
+    AdvocateDetailsFormView,
+    ChambersDetailsFormView,
+    LspDetailsFormView,
+    ParentProviderFormView,
+)
 
 
 def register_views():
     register_form_view(form_class=AddProviderForm, view_class=AddProviderFormView, blueprint=bp)
 
     register_form_view(form_class=LspDetailsForm, view_class=LspDetailsFormView, blueprint=bp)
+
+    register_form_view(form_class=AdvocateDetailsForm, view_class=AdvocateDetailsFormView, blueprint=bp)
 
     register_form_view(form_class=ChambersDetailsForm, view_class=ChambersDetailsFormView, blueprint=bp)
 
