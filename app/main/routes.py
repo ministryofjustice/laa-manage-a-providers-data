9cd85a5dfe0434259b04eed7b34253cc06009415
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-08-12 10:14:03 +0100

    MAPD-6: Entra ID Single Sign In Flow (#8)
    * Add authentication flow
    
    * WIP - state changes
    
    * Undo base template changes
    
    * Always use a fixed base image
    
    * Update talisman config to relax samesite policy
    
    * Fix failing unit tests
    
    * Remove test user
    
    * Add entra-id config
    
    * Add helm config for entra environment variables
    
    * Prefix entra id environment variables with ENTRA_ID
    
    * Use correct SESSION_REDIS config variable
    
    * Update redis config
    
    * Add return url on logout
    
    * Add custom sign out route
    
    * ENTRA_ID_SKIP_AUTH: env var to skip entra id auth on unsupported environments
    
    * ENTRA_ID_SKIP_AUTH: env var to skip entra id auth on unsupported environments
    
    * Skip entra auth for functional tests
    
    * Add session storage for tests
    
    * Add session storage for tests
    
    * Fix linting errors
    
    * Fix ruff errors
    
    * Fix ruff errors
    
    * Add documentation
    
    * Remove unused auth routes
    
    * Move user_context_processor to middleware file
    
    * Add user to sentry exceptions
    
    * Remove manually passing the user to the template. This is part of the current context and does not explicitly need to be passed
    
    * Set PREFERRED_URL_SCHEME to https so external links will be generated with the correct schema
    
    * Remove unused auth functions
    
    * Remove un-necessary auth_routes.py
    
    * Define logout URL scheme based on current app environment
    
    * Remove password based authentication
    
    * Add skip auth functionality
    
    * Navigate to index page on start of functionality tests
    
    * Rework skip auth unit tests
    
    * Rework skip auth unit tests
    
    * Merge conftests
    
    * Run live_server on a module scope to ensure tests do not pollute other modules
    
    * Log user login and logout
    
    * Dynamically select required URL scheme
    
    * Remove setting user in Sentry context
    
    * Log users UUID rather than the hash
    
    ---------
    
    Co-authored-by: said-moj <45761276+said-moj@users.noreply.github.com>

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,10 +2,10 @@ import math
 
 from flask import abort, current_app, render_template, request, url_for
 
-from app.auth import requires_authentication
+from app import auth
 from app.components.tables import DataTable, TableStructure, TransposedDataTable
-from app.main.utils import get_full_info_html
 from app.main import bp
+from app.main.utils import get_full_info_html
 
 
 @bp.get("/")
@@ -22,8 +22,8 @@ def status():
 
 
 @bp.get("/providers")
-@requires_authentication
-def providers():
+@auth.login_required
+def providers(context):
     def firm_name_html(row_data: dict[str, str]) -> str:
         _firm_id = row_data.get("firmId", "")
         _firm_name = row_data.get("firmName", "")
@@ -32,9 +32,6 @@ def providers():
     providers_shown_per_page = 20
 
     start_provider_firm_num = 0
-
-    if not current_app.config["TESTING"]:
-        start_provider_firm_num = 50647  # ID where the test data begins in PDA UAT.
 
     pda = current_app.extensions["pda"]
     data = pda.get_all_provider_firms()
@@ -70,8 +67,8 @@ def providers():
 
 
 @bp.get("/provider/<int:firm_id>/offices")
-@requires_authentication
-def offices(firm_id: int):
+@auth.login_required
+def offices(firm_id: int, context):
     def firm_office_html(row_data: dict[str, str]) -> str:
         _office_code = row_data.get("firmOfficeCode", "")
         return f"<a class='govuk-link' href='{url_for('main.contracts', firm_id=firm_id, office_code=_office_code)}'>{_office_code}</a>"
@@ -97,8 +94,8 @@ def offices(firm_id: int):
 
 
 @bp.get("/provider/<int:firm_id>/office/<string:office_code>/contracts")
-@requires_authentication
-def contracts(firm_id: int, office_code: str):
+@auth.login_required
+def contracts(firm_id: int, office_code: str, context):
     columns: list[TableStructure] = [
         {"text": "Category of Law", "id": "categoryOfLaw"},
         {"text": "Sub Category of Law", "id": "subCategoryLaw"},
@@ -126,8 +123,8 @@ def contracts(firm_id: int, office_code: str):
 
 
 @bp.get("/provider/<int:firm_id>/office/<string:office_code>/schedules")
-@requires_authentication
-def schedules(firm_id: int, office_code: str):
+@auth.login_required
+def schedules(firm_id: int, office_code: str, context):
     columns: list[TableStructure] = [
         {"text": "Contract Description", "id": "contractDescription"},
         {"text": "Contract Reference", "id": "contractReference"},
@@ -159,8 +156,8 @@ def schedules(firm_id: int, office_code: str):
 
 
 @bp.get("/provider/<int:firm_id>/office/<string:office_code>/bank-details")
-@requires_authentication
-def bank_details(firm_id: int, office_code: str):
+@auth.login_required
+def bank_details(firm_id: int, office_code: str, context):
     rows: list[TableStructure] = [
         {"text": "Vendor Site ID", "id": "vendorSiteId"},
         {"text": "Bank Name", "id": "bankName"},
--- tests/functional_tests/conftest.py
+++ tests/functional_tests/conftest.py
@@ -2,27 +2,6 @@ import pytest
 from flask import url_for
 
 
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {
-        **browser_context_args,
-        "viewport": {
-            "width": 1920,
-            "height": 1080,
-        },
-    }
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
-
-
 @pytest.fixture(scope="function", autouse=True)
-def startup(app, page):
+def startup(page):
     page.goto(url_for("main.index", _external=True))
