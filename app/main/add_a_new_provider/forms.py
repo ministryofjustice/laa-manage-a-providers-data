e728560a5d039e58e27f6a3f9c73eaa538d6f769
Stephanie de Jong <107569008+skdejong@users.noreply.github.com>
2025-08-14 17:43:09 +0100

    MAPD-43: Add draft chambers details page (#24)
    * MAPD-43: Add draft chambers details page
    
    * MAPD-43: Add caption, length validation, and update govuk components
    
    * MAPD-43: Move next_step_mapping to views.py
    
    * MAPD-43: Fix failing unit tests
    
    * MAPD-43: Modify error messages to match updated ticket from content design
    
    * MAPD-43: Set standard width for pageContent including ErrorSummary box
    
    * MAPD-43: Fix linting tests
    
    * Fix merge changes
    
    * Fix incorrect rebase
    
    * Fix incorrect merge changes
    
    * Remove unused ParentProviderForm
    
    * Fix formatting
    
    * Add feature tests
    
    * Update forms.py
    
    * Add caption and feature test. Update next_step_mapping

--- app/main/add_a_new_provider/forms.py
+++ app/main/add_a_new_provider/forms.py
@@ -1,4 +1,4 @@
-from flask import current_app
+from flask import current_app, session
 from wtforms import RadioField
 from wtforms.fields.simple import StringField
 from wtforms.validators import InputRequired, Length
@@ -38,11 +38,6 @@ class AddProviderForm(BaseForm):
 class LspDetailsForm(BaseForm):
     title = "Legal services provider details"
     url = "add-provider/lsp-details"
-
-
-class ChambersDetailsForm(BaseForm):
-    title = "Chambers details"
-    url = "add-provider/chambers-details"
 
 
 class AssignChambersForm(BaseForm):
@@ -122,3 +117,42 @@ class AssignChambersForm(BaseForm):
             )
 
         self.provider.choices = choices
+
+
+class ChambersDetailsForm(BaseForm):
+    title = "Chambers details"
+    url = "chambers-details"
+
+    @property
+    def caption(self):
+        return session.get("provider_name", default="unknown")
+
+    solicitor_advocate = RadioField(
+        "Is the provider a solicitor advocate?",
+        widget=GovRadioInput(heading_class="govuk-fieldset__legend--m", classes="govuk-radios--inline"),
+        validators=[InputRequired(message=("Select yes if the provider is a solicitor advocate"))],
+        choices=[
+            ("yes", "Yes"),
+            ("no", "No"),
+        ],
+    )
+
+    advocate_level = RadioField(
+        "Advocate level",
+        widget=GovRadioInput(heading_class="govuk-fieldset__legend--m"),
+        validators=[InputRequired(message=("Select the advocate level"))],
+        choices=[
+            ("pupil", "Pupil"),
+            ("junior", "Junior"),
+            ("king's counsel", "King's Counsel (KC, previously QC)"),
+        ],
+    )
+
+    bar_council_number = StringField(
+        "Bar Council roll number",
+        widget=GovTextInput(heading_class="govuk-fieldset__legend--m", classes="govuk-!-width-one-half"),
+        validators=[
+            InputRequired(message="Enter the Bar Council roll number"),
+            Length(max=15, message="Bar Council roll number must be 15 characters or less"),
+        ],
+    )
