363e692a9aede14475bfe067f2fc3956b3286f8a
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-08-14 17:54:27 +0100

    Re-add view provider route

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
 
+from app.main.views import ViewProvider
+
 bp = Blueprint("main", __name__, template_folder="templates")
 
 # Import modules after blueprint creation to avoid circular imports
 from app.main import middleware  # noqa: E402,F401
 from app.main import routes  # noqa: E402,F401
 from app.main.add_a_new_provider import register_views  # noqa: E402,F401
+
+bp.add_url_rule("/view-provider", view_func=ViewProvider.as_view("view_provider"))
