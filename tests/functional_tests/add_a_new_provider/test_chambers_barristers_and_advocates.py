b542cf3960e01d4af3644bf0f3a7e48a9a1cea1f
Adrian Tucker <adrian.tucker@digital.justice.gov.uk>
2025-09-11 15:46:50 +0100

    tests: Add functional tests for barrister and advocate listing

---
+++ tests/functional_tests/add_a_new_provider/test_chambers_barristers_and_advocates.py
@@ -0,0 +1,28 @@
+import pytest
+from flask import url_for
+from playwright.sync_api import Page, expect
+
+
+def test_barristers_and_advocates_visible(page: Page) -> None:
+    page.get_by_role("button", name="Start now").click()
+    page.get_by_role("button", name="Search").click()
+    page.get_by_role("link", name="JOHNSON LEGAL SERVICES").click()
+    expect(page.get_by_role("link", name="Barristers and advocates")).to_be_visible()
+    page.get_by_role("link", name="Barristers and advocates").click()
+    expect(page.get_by_role("cell", name="Karen Sillen")).to_be_visible()
+    expect(page.get_by_role("cell", name="Sophie Lelièvre")).to_be_visible()
+    expect(page.get_by_role("button", name="Add another barrister")).to_be_visible()
+    expect(page.get_by_role("cell", name="DAVIES & ASSOCIATES")).to_be_visible()
+    expect(page.get_by_role("cell", name="Finn O'Connor")).to_be_visible()
+    expect(page.get_by_role("button", name="Add another advocate")).to_be_visible()
+
+
+def test_correct_message_when_no_barristers_or_advocates(page: Page) -> None:
+    page.get_by_role("button", name="Start now").click()
+    page.get_by_role("button", name="Search").click()
+    page.get_by_role("link", name="NORTHERN LEGAL CHAMBERS").click()
+    page.get_by_role("link", name="Barristers and advocates").click()
+    expect(page.locator("#main-content")).to_contain_text("No barristers assigned to this chambers")
+    expect(page.locator("#main-content")).to_contain_text("No advocates assigned to this chambers")
+    expect(page.get_by_role("button", name="Add a barrister")).to_be_visible()
+    expect(page.get_by_role("button", name="Add an advocate")).to_be_visible()
