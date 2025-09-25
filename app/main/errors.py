a95cf89d783e38971f1606b018ee25643e76cef3
Stephanie de Jong <107569008+skdejong@users.noreply.github.com>
2025-09-25 11:32:38 +0100

    MAPD-185: Add Sentry capture message to errors.py file

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,10 +1,12 @@
-from flask import render_template
+import sentry_sdk
+from flask import render_template, request
 
 from app.main import bp
 
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
+    sentry_sdk.capture_message(f"404 not found - {request.path}")
     return render_template("errors/404.html"), 404
 
 
