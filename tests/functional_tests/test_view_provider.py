4f74c0b77ab9f5088269e118ff37065708dc259a
said-moj <45761276+said-moj@users.noreply.github.com>
2025-09-25 11:21:26 +0100

    Use govuk components

--- tests/functional_tests/test_view_provider.py
+++ tests/functional_tests/test_view_provider.py
@@ -14,7 +14,15 @@ def test_view_provider_page_ui_loads(page):
     expect(page.get_by_role("link", name="Offices")).to_be_visible()
 
     # Tags
-    expect(page.locator("strong", has_text="Inactive")).to_be_visible()
+    expect(page.locator(".govuk-tag", has_text="Inactive")).to_be_visible()
+    expect(
+        page.locator(".govuk-warning-text__text", has_text="Provider marked as inactive on 25/09/2025")
+    ).to_be_visible()
+    expect(
+        page.locator(
+            ".govuk-warning-text__text", has_text="Payments for all offices are hold because provider is inactive"
+        )
+    ).to_be_visible()
 
     # Buttons
     expect(page.get_by_role("button", name="Make provider active")).to_be_visible()
