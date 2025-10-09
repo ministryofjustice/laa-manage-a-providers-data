dd48afb1a9ec5c756e051055bf222e5951859a1d
Adrian Tucker <adrian.tucker@digital.justice.gov.uk>
2025-10-09 10:23:02 +0100

    fix: Resolve bad merge

--- app/main/utils.py
+++ app/main/utils.py
@@ -389,7 +389,7 @@ def get_firm_tags(firm: Firm):
     tags: list[Tag] = []
     if firm.inactive_date:
         tags.append(Tag(TagType.INACTIVE))
-    if office.hold_all_payments_flag == "Y":
+    if firm.hold_all_payments_flag == "Y":
         tags.append(Tag(TagType.ON_HOLD))
     return tags
 
@@ -398,7 +398,7 @@ def get_office_tags(office: Office):
     tags: list[Tag] = []
     if office.inactive_date:
         tags.append(Tag(TagType.INACTIVE))
-    if firm.hold_all_payments_flag == "Y":
+    if office.hold_all_payments_flag == "Y":
         tags.append(Tag(TagType.ON_HOLD))
     return tags
 
