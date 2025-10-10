6ebb6c6c8eb12988df025c21f1c67dd375d523c9
Stephanie de Jong <107569008+skdejong@users.noreply.github.com>
2025-10-10 10:00:20 +0100

    MAPD-221: Create office change form and update change links

--- app/pda/mock_api.py
+++ app/pda/mock_api.py
@@ -798,3 +798,7 @@ class MockProviderDataApi:
 
         # Create the new bank account and assign it to the office
         return self.create_office_bank_account(firm_id, office_code, new_bank_account)
+
+    def update_office_contact_details(self, firm_id, firm_office_code, payload):
+        office_data = self._find_office_data(firm_id, firm_office_code)
+        office_data.update(payload)
