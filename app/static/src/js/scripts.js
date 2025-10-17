7256c885f3bf33b965246e5a6819742ade8d5523
Stephanie de Jong <107569008+skdejong@users.noreply.github.com>
2025-10-17 16:25:15 +0100

    MAPD-221: Add focus error summary

--- app/static/src/js/scripts.js
+++ app/static/src/js/scripts.js
@@ -1,6 +1,7 @@
-import * as GOVUKFrontend from 'govuk-frontend/dist/govuk/all.bundle.js';
-import * as MOJFrontend from '@ministryofjustice/frontend/moj/moj-frontend.min.js';
-GOVUKFrontend.initAll()
+import * as GOVUKFrontend from "govuk-frontend/dist/govuk/all.bundle.js";
+import * as MOJFrontend from "@ministryofjustice/frontend/moj/moj-frontend.min.js";
+GOVUKFrontend.initAll();
 MOJFrontend.initAll();
 
-import './back_link';
+import "./back_link";
+import "./focus-error-summary";
