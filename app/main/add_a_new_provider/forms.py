d9ad2df90be19c0feba74a03df49c58946fb529d
said-moj <45761276+said-moj@users.noreply.github.com>
2025-10-22 15:03:20 +0100

    Update labels

--- app/main/add_a_new_provider/forms.py
+++ app/main/add_a_new_provider/forms.py
@@ -408,7 +408,7 @@ class AddBarristerDetailsForm(BaseForm):
 class AddAdvocateBarristerCheckForm(BaseForm):
     template = "add_provider/barrister-check-form.html"
     same_liaison_manager_as_chambers = RadioField(
-        label="Do you want to use the same liaison manager as the chambers",
+        label="Do you want to use the same liaison manager as the chambers?",
         choices=YES_NO_CHOICES,
         widget=GovRadioInput(heading_class="govuk-fieldset__legend--m"),
         validators=[InputRequired(message="Select yes if you want to use the same liaison manager as the chambers")],
