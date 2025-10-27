d6cd192a50bddde271e59c3a59ac43017a0aa12b
said-moj <45761276+said-moj@users.noreply.github.com>
2025-10-27 16:15:14 +0000

    Remove 'office name/head office' question page from 'add an office flow'

--- app/main/add_a_new_office/views.py
+++ app/main/add_a_new_office/views.py
@@ -1,40 +1,8 @@
-from flask import Response, abort, redirect, render_template, session, url_for
+from flask import Response, redirect, render_template, session, url_for
 
-from app.forms import BaseForm
 from app.main.utils import add_new_office
 from app.models import Firm, Office
 from app.views import BaseFormView
-
-
-class AddOfficeFormView(BaseFormView):
-    """Form view for the Add a new office page"""
-
-    def get_success_url(self, form: BaseForm | None = None) -> str:
-        if not form or not hasattr(form, "firm"):
-            abort(404)
-
-        return url_for("main.add_office_contact_details", firm=form.firm)
-
-    def form_valid(self, form):
-        session["new_office"] = {
-            "office_name": form.data.get("office_name"),
-            "is_head_office": form.data.get("is_head_office"),
-        }
-
-        return super().form_valid(form)
-
-    def get(self, context, firm: Firm, **kwargs):
-        form = self.get_form_class()(firm=firm)
-
-        return render_template(self.template, **self.get_context_data(form, **kwargs))
-
-    def post(self, firm: Firm, *args, **kwargs) -> Response | str:
-        form = self.get_form_class()(firm=firm)
-
-        if form.validate_on_submit():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form, **kwargs)
 
 
 class OfficeContactDetailsFormView(BaseFormView):
@@ -44,13 +12,12 @@ class OfficeContactDetailsFormView(BaseFormView):
         return url_for("main.view_office", firm=firm, office=new_office)
 
     def form_valid(self, form):
-        # Check if office data exists in session
-        if not session.get("new_office"):
-            abort(500)
-
+        head_office = self.get_api().get_head_office(form.firm.firm_id)
         # Add contact details to the existing office dict
         session["new_office"].update(
             {
+                "office_name": form.firm.firm_name,
+                "is_head_office": not head_office,
                 "address_line_1": form.data.get("address_line_1"),
                 "address_line_2": form.data.get("address_line_2"),
                 "address_line_3": form.data.get("address_line_3"),
@@ -75,10 +42,6 @@ class OfficeContactDetailsFormView(BaseFormView):
         return redirect(self.get_success_url(new_office, form.firm))
 
     def get(self, context, firm: Firm, **kwargs):
-        # Check if office data exists in session
-        if not session.get("new_office"):
-            abort(400)
-
         form = self.get_form_class()(firm=firm)
         return render_template(self.template, **self.get_context_data(form, **kwargs))
 
