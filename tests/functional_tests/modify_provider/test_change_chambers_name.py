832ac04b2405a2253688557dcb033562e83e7dc0
Adrian Tucker <adrian.tucker@digital.justice.gov.uk>
2025-11-24 22:00:41 +0000

    tests: Change chambers name

---
+++ tests/functional_tests/modify_provider/test_change_chambers_name.py
@@ -0,0 +1,41 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+from tests.functional_tests.utils import navigate_to_provider_page
+
+
+@pytest.mark.usefixtures("live_server")
+def test_change_chambers_name(page: Page):
+    navigate_to_provider_page(page, "JOHNSON LEGAL SERVICES")
+    page.get_by_role("link", name="Change provider name").click()
+    expect(page.get_by_role("textbox", name="New chambers name")).to_have_value("JOHNSON LEGAL SERVICES")
+    page.get_by_role("textbox", name="New chambers name").fill("Functional test provider")
+    page.get_by_role("button", name="submit").click()
+
+    expect(page.get_by_role("heading", name="Functional test provider")).to_be_visible()
+    expect(page.get_by_text("Chambers name successfully updated")).to_be_visible()
+
+
+@pytest.mark.usefixtures("live_server")
+def test_change_chambers_name_no_change(page: Page):
+    navigate_to_provider_page(page, "JOHNSON LEGAL SERVICES")
+    page.get_by_role("link", name="Change provider name").click()
+    expect(page.get_by_role("textbox", name="New chambers name")).to_have_value("JOHNSON LEGAL SERVICES")
+    page.get_by_role("button", name="submit").click()
+
+    expect(page.get_by_role("heading", name="JOHNSON LEGAL SERVICES")).not_to_be_visible()
+    expect(page.get_by_role("heading", name="Change chambers name")).to_be_visible()
+    expect(
+        page.get_by_role("link", name="You have not changed the chambers name. Cancel if you do not want to change it.")
+    ).to_be_visible()
+
+
+@pytest.mark.usefixtures("live_server")
+def test_change_chambers_name_cancel(page: Page):
+    navigate_to_provider_page(page, "JOHNSON LEGAL SERVICES")
+    page.get_by_role("link", name="Change provider name").click()
+    expect(page.get_by_role("textbox", name="New chambers name")).to_have_value("JOHNSON LEGAL SERVICES")
+    page.get_by_role("link", name="Cancel").click()
+
+    expect(page.get_by_role("heading", name="JOHNSON LEGAL SERVICES")).to_be_visible()
+    expect(page.get_by_text("Chambers name successfully updated")).not_to_be_visible()
