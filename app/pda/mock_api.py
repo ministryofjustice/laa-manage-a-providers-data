f437e65c46070bfbcdfc815831c0bf8c31fc3079
said-moj <45761276+said-moj@users.noreply.github.com>
2025-12-15 13:42:51 +0000

    Remove unused method

--- app/pda/mock_api.py
+++ app/pda/mock_api.py
@@ -851,11 +851,6 @@ class MockProviderDataApi:
     def update_office_false_balance(self, firm_id: int, office_code: str, data: dict) -> Office:
         return self.patch_office(firm_id, office_code, data)
 
-    def update_office_debt_recovery(self, firm_id: int, office_code: str, data: dict) -> Office:
-        office_data = self._find_office_data(firm_id, office_code)
-        office_data.update(data)
-        return Office(**_clean_data(office_data))
-
     def get_list_of_contract_manager_names(self):
         # Static list of 12 fake contract managers
         return [
